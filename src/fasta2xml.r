56df702d9e04e867e090a7da99b67c982bbe904e
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-08-10 16:24:23 +1200

    Can now specify datatype

--- src/fasta2xml.r
+++ src/fasta2xml.r
@@ -1,16 +1,26 @@
 library("beter")
+library("argparser")
+
+
+args_parser = function(){
+    parser = arg_parser("Produce a BEAST XML from a beter XML template and a fasta file.")
+    parser = add_argument(parser, "template", type="character", help="A `beter` XML template")
+    parser = add_argument(parser, "fasta", type="character", help="A fasta file")
+    parser = add_argument(parser, "output", type="character", help="Beast XML file with sequences")
+    parser = add_argument(parser, "--datatype", type="character", help="Beast sequence data type.")
+    args = parse_args(parser)
+    subset(args, !names(args) %in% c("", help, "opts"))
+    }
+
+main = function(template, fasta, output, datatype="standard"){
+    alignment_id = basename(tools::file_path_sans_ext(output))
+    beter::process_template(
+        template, output, fasta,
+        parameters = list("datatype" = datatype, "alignment_id" = alignment_id)
+        )
+    }
 
 if(sys.nframe() == 0){
-    args = commandArgs(TRUE)
-
-    alignment_id = basename(tools::file_path_sans_ext(args[3]))
-    beter::process_template(
-        template = args[1],
-        output = args[3],
-        alignment = args[2],
-        parameters = list(
-            "datatype" = "nucleotideDiploid16",
-            "alignment_id" = alignment_id
-            )
-        )
+    args = args_parser()
+    do.call(main, args)
     }
