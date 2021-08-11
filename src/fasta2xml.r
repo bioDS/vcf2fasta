ed8557c67af3d0d44a9cdf0a98d2bbbe423bc54f
Jiří Moravec <jiri.c.moravec@gmail.com>
2021-08-11 16:41:10 +1200

    Added default value for optional parameter

--- src/fasta2xml.r
+++ src/fasta2xml.r
@@ -7,15 +7,16 @@ args_parser = function(){
     parser = add_argument(parser, "template", type="character", help="A `beter` XML template")
     parser = add_argument(parser, "fasta", type="character", help="A fasta file")
     parser = add_argument(parser, "output", type="character", help="Beast XML file with sequences")
-    parser = add_argument(parser, "--datatype", type="character", help="Beast sequence data type.")
+    parser = add_argument(parser, "--datatype", default="standard",
+        help="Beast sequence data type.")
     args = parse_args(parser)
-    subset(args, !names(args) %in% c("", help, "opts"))
+    subset(args, !names(args) %in% c("", "help", "opts"))
     }
 
 main = function(template, fasta, output, datatype="standard"){
     alignment_id = basename(tools::file_path_sans_ext(output))
     beter::process_template(
-        template, output, fasta,
+        template, output, alignment=fasta,
         parameters = list("datatype" = datatype, "alignment_id" = alignment_id)
         )
     }
